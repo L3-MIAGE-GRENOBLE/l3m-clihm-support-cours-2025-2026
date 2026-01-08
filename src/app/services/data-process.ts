d4577597aff5901b8247f1eb0e789f50c613edca
alexandre.demeure@univ-grenoble-alpes.fr <alexandre.demeure@univ-grenoble-alpes.fr>
2026-01-08 16:46:11 +0100

    fin cours 1

--- src/app/services/data-process.ts
+++ src/app/services/data-process.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { strCollegesCSV } from '../data/data.college.csv';
-import { processStringToDataCollege } from '../data/college.tuple';
+import { DataCollege, processStringToDataCollege } from '../data/college.tuple';
 import { processCollegeFromTuple } from '../data/college.interface';
 
 @Injectable({
@@ -10,6 +10,21 @@ export class DataProcess {
   
   constructor() {
     console.log("DataProcess::constructor (fichier ./src/app/services/data-process.ts)");
+    const listColleges = strCollegesCSV
+      .split("\n")
+      .map(processStringToDataCollege)
+      .map(processCollegeFromTuple)
     
+    console.log(
+      "la liste complète:",
+      listColleges,
+      "\nla liste des REP PRIVE",
+      listColleges.filter(e => e.nombre_eleves_total < 50),
+      "nombre d'élèves en 2020 :",
+      listColleges.reduce(
+        (nb, c) => c.rentree_scolaire === 2020 ? nb + c.nombre_eleves_total : nb,
+        0
+      )
+    )
   }
 }
